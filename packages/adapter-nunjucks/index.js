9e38df716f25c6b0be13b9879dfe4f1d6c2e0ad1
Mark Perkins <mark@allmarkedup.com>
2019-02-20 21:41:23 +0100

    appConfig is no longer supplied

--- packages/adapter-nunjucks/index.js
+++ packages/adapter-nunjucks/index.js
@@ -9,7 +9,7 @@ module.exports = function(opts = {}) {
   opts = defaultsDeep(opts, defaults);
   const adapter = createAdapter(opts);
 
-  return function nunjucksAdapter(app, compiler, appConfig) {
+  return function nunjucksAdapter(app, compiler) {
     // Compiler middleware to identify .njk files as HTML fragments so that
     // further pre-processing of templates can take place later if required.
     compiler.use(components => {
