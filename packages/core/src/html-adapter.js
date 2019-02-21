f329ec276877133bc409a832d9734481d4acb939
Mark Perkins <mark@allmarkedup.com>
2019-02-21 22:42:47 +0100

    Fix default HTML adapter, add html fragment flag compiler middleware

--- packages/core/src/html-adapter.js
+++ packages/core/src/html-adapter.js
@@ -1,11 +1,21 @@
 const pupa = require('pupa');
 
-module.exports = function(app, config) {
+module.exports = function(app, compiler) {
   const views = ['view.html', '{name}.view.html'];
+
+  compiler.use(components => {
+    components.forEach(component => {
+      const fragments = component.matchFiles(views);
+      fragments.forEach(file => {
+        file.isHTMLFragment = true;
+      });
+    });
+  });
+
   return {
     views,
     async render(component, props, state) {
-      const view = component.matchFile(views);
+      const view = component.matchFiles(views)[0];
       if (view) {
         return pupa(await view.getContents(), props);
       }
