58ca077c51c724608d7e30b4ce5e455c38288d1a
Mark Perkins <mark@allmarkedup.com>
2019-02-22 16:15:48 +0100

    Keep src-relative path on files available

--- packages/core/src/middleware/files.js
+++ packages/core/src/middleware/files.js
@@ -5,6 +5,7 @@ module.exports = function() {
   return function filesMiddleware(components) {
     components.forEach(component => {
       component.files = component.files.map(file => {
+        file.srcRelative = file.relative;
         file.relative = slash(relative(component.root.path, file.path));
         Object.defineProperty(file, 'handle', {
           get() {
