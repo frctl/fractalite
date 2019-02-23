dcba25358a45de394daf40a8af0e1d1cef70005d
Mark Perkins <mark@allmarkedup.com>
2019-02-23 14:50:23 +0100

    Serve index if no other file matches

--- packages/app/src/serve.js
+++ packages/app/src/serve.js
@@ -1,11 +1,16 @@
 const Koa = require('koa');
 const serve = require('koa-static');
+const send = require('koa-send');
 const { normalizePath } = require('@frctl/fractalite-support/utils');
 
 module.exports = async function(dir, port = 0) {
-  dir = normalizePath(dir);
+  const serveOpts = {
+    root: normalizePath(dir)
+  };
+
   const app = new Koa();
-  app.use(serve(dir));
+  app.use(serve(serveOpts.root, serveOpts));
+  app.use(ctx => send(ctx, 'index.html', serveOpts)); // file not found, serve index
 
   return await new Promise((resolve, reject) => {
     const httpServer = app.listen(port, err => (err ? reject(err) : resolve(httpServer)));
