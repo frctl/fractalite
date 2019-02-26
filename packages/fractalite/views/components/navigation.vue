fa67e7596ce4e1bbf9d48860b216db6df11b3fdd
Mark Perkins <mark@allmarkedup.com>
2019-02-26 12:12:51 +0100

    More theming

--- packages/fractalite/views/components/navigation.vue
+++ packages/fractalite/views/components/navigation.vue
@@ -1,7 +1,11 @@
 <ul class="fr-nav__items" :data-level="depth">
   <li v-for="item in items">
-    <app-link :to="item.url" v-if="item.url" class="fr-nav__label fr-nav__link" v-html="item.label"></app-link>
-    <span class="fr-nav__label fr-nav__toggle" v-html="item.label" v-else></span>
+    <app-link :to="item.url" v-if="item.url" class="fr-nav__link">
+      <span class="fr-nav__label" v-html="item.label"></span>
+    </app-link>
+    <span class="fr-nav__toggle" v-else>
+      <span class="fr-nav__label" v-html="item.label"></span>
+    </span>
     <navigation :pages="item.children" :depth="depth + 1" v-if="item.children"></navigation>
   </li>
 </ul>
