fec36e4b5ade15fe178f994342105dc527836701
Mark Perkins <mark@allmarkedup.com>
2019-03-10 16:58:29 +0100

    Add support for component

--- packages/plugin-json-schema/index.js
+++ packages/plugin-json-schema/index.js
@@ -13,30 +13,45 @@ module.exports = function(opts = {}) {
       let ajvOpts = opts.validate === true ? {} : opts.validate;
 
       compiler.use((components, next, ctx) => {
+        // build a set of definitions comprising of all component schemas
+        const definitions = { $id: 'components' };
+        components.forEach(component => {
+          definitions[component.name] = {
+            type: 'object',
+            ...component.config.schema
+          };
+        });
+
         components.forEach(component => {
           if (component.config.schema) {
-            const ajv = new Ajv({
-              allErrors: true,
-              ...ajvOpts
-            });
-            const schema = {
-              $schema: 'http://json-schema.org/draft-07/schema#',
-              $id: component.name,
-              title: component.label,
-              type: 'object',
-              ...component.config.schema
-            };
+            try {
+              const ajv = new Ajv({
+                allErrors: true,
+                ...ajvOpts
+              });
+              ajv.addSchema(definitions);
 
-            component.scenarios.forEach(scenario => {
-              const valid = ajv.validate(schema, scenario.props);
-              if (!valid) {
-                const { errors } = ajv;
-                for (const error of errors) {
-                  const message = error.dataPath ? `${error.dataPath} ${error.message}` : error.message;
-                  ctx.log(`${component.name}/${scenario.name} ${message}`, 'warn');
+              const schema = {
+                $schema: 'http://json-schema.org/draft-07/schema#',
+                $id: component.name,
+                title: component.label,
+                type: 'object',
+                ...component.config.schema
+              };
+
+              component.scenarios.forEach(scenario => {
+                const valid = ajv.validate(schema, scenario.props);
+                if (!valid) {
+                  const { errors } = ajv;
+                  for (const error of errors) {
+                    const message = error.dataPath ? `${error.dataPath} ${error.message}` : error.message;
+                    ctx.log(`${component.name}/${scenario.name} ${message}`, 'warn');
+                  }
                 }
-              }
-            });
+              });
+            } catch (err) {
+              ctx.log(err);
+            }
           }
         });
       });
